d5ce5aa86ead14308fd9bb685d6a2bc52e426b2e
jsstoni <jsstoniha@gmail.com>
2023-09-22 12:49:40 -0300

    vue-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+import routes from "./routes";
 
-createApp(App).mount('#app')
+const vueApp = createApp(App);
+vueApp.use(routes);
+vueApp.mount("#app");
