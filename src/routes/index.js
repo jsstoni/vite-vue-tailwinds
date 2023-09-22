812489b11afc37a4b01117c050167a9e40f3ac5c
jsstoni <jsstoniha@gmail.com>
2023-09-22 12:54:28 -0300

    router - views

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,13 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [];
+const Home = () => import("../views/Home.vue");
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
