2048bc3386e2913a6ab25c0d613bf6d98cff3907
jsstoni <jsstoniha@gmail.com>
2023-09-23 15:00:17 -0300

    dark mode

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,18 @@
+<script setup>
+import { useDark, useToggle } from "@vueuse/core";
+import { computed } from "vue";
+
+const dark = useDark();
+const toggleDark = useToggle(dark);
+
+const isDark = computed(() => (dark.value ? "Dark" : "White"));
+</script>
+
 <template>
-  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <button @click="toggleDark()">{{ isDark }}</button>
+  <h1
+    class="text-3xl font-bold underline bg-white text-black dark:bg-black dark:text-white"
+  >
+    Hello world!
+  </h1>
 </template>
