bd1965301ad61f987e5d0b5fa38307a385c859dd
jsstoni <jsstoniha@gmail.com>
2023-09-26 15:58:50 -0300

    support store with pinia  and PPS

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { createApp } from "vue";
 import "@/style.css";
 import App from "@/App.vue";
 import routes from "@/routes";
+import stores from "@/stores";
 
 const vueApp = createApp(App);
+vueApp.use(stores);
 vueApp.use(routes);
 vueApp.mount("#app");
